b564362ef328bd65069c9a10de026c7cb6b84119
Damilola Joshua <damilolajoshua2021@gmail.com>
2024-11-28 17:18:28 -0800

    use of useTransition()

--- src/App.js
+++ src/App.js
@@ -1,41 +1,26 @@
 import './App.css';
-import { PostForm } from './components/PostForm';
-// import './appStyles.css'
-// import styles from './appStyles.module.css'
-// import { ClickHandler } from './components/clickHandler';
-// import { NameList } from './components/NameList';
-// import { Stylesheet } from './components/Stylesheet';
-// import { Inline } from './components/inline';
-// import { Greet } from './components/greet';
-// import { Message } from './components/message';
-// import { ParentComponent } from './components/ParentComponent';
-// import { UserGreeting } from './components/UserGreeting';
-import { Form } from './components/form';
-import { PostList } from './components/PostList';
+import NAMES from './components/data.json'
+import { useState, useTransition } from 'react';
 
 function App() {
+  const [query, setQuery] = useState('')
+  const [inputValue, setInputValue] = useState('')
+  const [isPending, startTransition] = useTransition()
+  const changeHandler = (event) => {
+    setInputValue(event.target.value)
+    startTransition(() => setQuery(event.target.value))
+  }
+  const filteredNames = NAMES.filter(item => {
+    return item.first_name.includes(query) || item.last_name.includes(query)
+  })
   return (
-    <div className="App">
-      {/* <Greet name='Druce' heroName='Batman' />
-      <Greet name='Clark' heroName='Ironman' />
-      <Greet name='Diana' heroName='Spiderman' >
-        <button>Action</button>
-      </Greet>
-      <Greet name='Vishwaz' heroName='WonderWoman' >
-        <p>This is Children Props</p>
-      </Greet> */}
-      {/* <Message />
-      <ClickHandler /> */}
-      {/* <ParentComponent /> */}
-      {/* <UserGreeting />
-      <NameList />
-      < Stylesheet /> */}
-      {/* <Inline />
-      <h1 className="error">Error</h1>
-      <h1 className={styles.success}>Success</h1> */}
-      <Form />
-      {/* <PostForm />
-      <PostList /> */}
+    <div className="App"> 
+      <input type="text" value={inputValue} onChange={changeHandler}/>
+      {isPending && <p>Updating list...</p>}
+      {filteredNames.map((item) => (
+        <p key={item.id}>{item.first_name} {item.last_name}</p>
+      ))
+    }
     </div>
   )
 }
